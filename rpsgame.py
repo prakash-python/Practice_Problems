9b0d4dd70d44a27f1f008fef386c4d45f171b2a9
prakash-python <daramprakashkumar587@gmail.com>
2025-07-18 22:35:03 +0530

    rpsgame modified

--- rpsgame.py
+++ rpsgame.py
@@ -1,40 +1,57 @@
 import random
-options=['rock','paper','scissor']
-win_count=0
-lose_count=0
-no_of_games=0
-play=True
-while play:
-    computer=random.choice(options)
-    player=None
-    player=input('Enter your choice [rock,paper,scissor] : ')
-    while player not in options:
-        print('Invalid choice, please try again.')
-        player=input('Enter your choice [rock,paper,scissor] : ')
-    no_of_games += 1
-    if player==computer:
+choices = ['rock','paper','scissor']
+t_count =0
+w_count = 0
+g_count = 0
+i_count = 0 
+is_true = True
+while is_true:
+    user = input('Enter Your choices \n\t Your choices are [Rock,Paper,Scissor] : ').lower()
+    computer = random.choice(choices)
+    g_count += 1
+    while user not in choices:
         
-        print('It is a Tie..')
-    elif player=='rock' and computer=='paper':
-        win_count+=1
-        print('You Win!')
-    elif player=='paper' and computer=='scissor':
-        win_count+=1
-        print('You Win!')
-    elif player=='scissor' and computer=='rock':
-        win_count+=1
-        print('You Win!')
+        user = input('Invalid Choice! \n\t Please enter a valid choice \n\t Your choices are [Rock,Paper,Scissor] : ')
+        i_count += 1
+        g_count += 1
+        continue
+    
+    if user == computer:
+        print('It is Tie')
+        t_count += 1
+    elif user == 'rock' and computer == 'scissor':
+        print('You win the game ')
+        w_count += 1
+    elif user == 'paper' and computer == 'rock':
+        print('You win the game')
+        w_count += 1
+    elif user == 'scissor' and computer == 'paper':
+        print('you win the game')
+        w_count += 1
     else:
-        lose_count+=1
-        print('You Lose....')
-    if not input('you want to play agian!?(y/n): ').lower() == 'y':
-        play=False
-else:
-    ties = no_of_games - win_count - lose_count
-    print('Game Over!')
-    print('Thanks for playing ')
-    print('Your Score:')
-    print('Wins:', win_count)
-    print('Losses:', lose_count)
-    print('Ties:', ties)
-    print('Total Games:', win_count + lose_count + ties)
+        print('You loose the game')
+    is_valid = True
+    while is_valid:
+        
+        u_input = input('Do you want to play again ! (Y/N) : ').lower()
+        while u_input != 'n' and u_input != 'y':
+            
+            u_input = input('Ivalid Choice ! \n\t Please Enter a valid choice \n\t Your choices are (Y/N) : ')
+            is_valid = True
+            continue
+        else:
+            if u_input == 'n':
+                
+                is_true = False
+                break
+            elif u_input == 'y':
+                is_true = True
+                break
+            
+
+print('Thank You for playing with me')
+print('Total games you have played ',g_count)
+print('Win score ',w_count)
+print('Loose score ',g_count-w_count-t_count-i_count)
+print('Tie Score ',t_count)
+print('Invalid Choices ',i_count)
